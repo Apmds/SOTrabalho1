0669dd12d8f1a34c00122a29fd469bacd9782a7d
Gonçalo Silva <goncaloliveirasilva@gmail.com>
2024-11-14 11:48:05 +0000

    Recursividade a funcionar

--- backup_check.sh
+++ backup_check.sh
@@ -14,47 +14,66 @@ function startupChecks() {
     [[ -d "$BACKUP_DIR" ]] || { echo "Backup directory $BACKUP_DIR does not exist!"; exit 1; }
 }
 
-function main() {
-    startupChecks "$@"
 
-    num_files_work=$(ls -1q "$WORK_DIR" | wc -l)
-    num_files_backup=$(ls -1q "$BACKUP_DIR" | wc -l)
+function check() {
 
-    if [[ "$num_files_backup" -eq 0 && "$num_files_work" -eq 0 ]]; then
-        exit 0
-    fi
+    local LOOP_DIR=$1
+    local NOT_LOOP_DIR=$2
 
-    LOOP_DIR="$BACKUP_DIR"
-    NOT_LOOP_DIR="$WORK_DIR"
+    # num_files_work=$(ls -1q "$WORK_DIR" | wc -l)
+    # num_files_backup=$(ls -1q "$BACKUP_DIR" | wc -l)
 
-    if [[ "$num_files_backup" -eq 0 ]]; then
-        LOOP_DIR="$WORK_DIR"
-        NOT_LOOP_DIR="$BACKUP_DIR"
-    fi
+    # if [[ "$num_files_backup" -eq 0 && "$num_files_work" -eq 0 ]]; then
+    #     echo "The number of files in directory $WORK_DIR and directory $BACKUP_DIR differs"
+    #     exit 0
+    # fi
+
+    # LOOP_DIR="$BACKUP_DIR"
+    # NOT_LOOP_DIR="$WORK_DIR"
+
+    # if [[ "$num_files_backup" -eq 0 ]]; then
+    #     LOOP_DIR="$WORK_DIR"
+    #     NOT_LOOP_DIR="$BACKUP_DIR"
+    # fi
 
     for file in "$LOOP_DIR"/*; do
         if [[ ! -e "$file" ]]; then
-            echo Diretório vazio.
+            echo "Empty directory"
             break
         fi
         
-        file_other="${file%/*}"
+        local file_other="${file%/*}"
         file_other="${file_other//$LOOP_DIR/$NOT_LOOP_DIR}"
         file_other="$file_other/${file##*/}"
 
+        if [[ -d "$file" ]]; then
+            if [[ -d "$file_other" ]]; then
+                check "$file" "$file_other"
+            else
+                echo "Directory $file_other does not exist"
+            fi
+            continue
+        fi
+
         if [[ ! -e "$file_other" ]]; then
-            echo "$file_other" não existe.
+            echo ""$file_other" does not exist"
             continue
         fi
 
         # Dá sempre igual?? Afinal não
-        hash=($(md5sum "$file"))
-        hash_other=($(md5sum "$file_other"))
+        local hash=($(md5sum "$file"))
+        local hash_other=($(md5sum "$file_other"))
         
         if [[ "$hash" != "$hash_other" ]]; then
             echo "$file" "$file_other" differ.
         fi
     done
+
+}   
+
+function main() {
+    startupChecks "$@"
+    check "$@"
 }
 
 main "$@"
